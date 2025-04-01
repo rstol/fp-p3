2dc3e6466a48f2bc7212418e66228853a23ff701
Romeo Stoll <47355990+rstol@users.noreply.github.com>
2025-04-01 19:56:55 +0200

    team selection

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -6,6 +6,7 @@ import { BASE_URL } from '~/lib/const';
 import type { Team, Game } from '~/types/data';
 import type { Route } from './+types/home';
 import GamesTable from '~/components/GamesTable';
+import Header from '~/components/Header';
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -34,23 +35,27 @@ clientLoader.hydrate = true;
 
 export default function Home() {
   return (
-    <div className="mx-auto max-w-7xl space-y-10 px-4 py-6">
-      <h1 className="text-xl font-semibold">Dashboard</h1>
-      <ResizablePanelGroup direction="horizontal" className="min-h-[500px]">
-        <ResizablePanel id="left-panel" defaultSize={70}>
-          <ScatterPlot />
-        </ResizablePanel>
-        <ResizableHandle withHandle />
-        <ResizablePanel defaultSize={30}>
-          <div className="flex h-full items-center justify-center p-6">
-            <span className="font-semibold">Basketball Court Play view</span>
-            {/* TODO render play  */}
-          </div>
-        </ResizablePanel>
-        <ResizableHandle />
-      </ResizablePanelGroup>
-      <TeamsTable />
-      <GamesTable />
+    <div className="mx-auto max-w-7xl px-4 py-6">
+      <div className="space-y-2">
+        <Header />
+        <ResizablePanelGroup direction="horizontal" className="min-h-[500px]">
+          <ResizablePanel id="left-panel" defaultSize={70}>
+            <ScatterPlot />
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel defaultSize={30}>
+            <div className="flex h-full items-center justify-center p-6">
+              <span className="font-semibold">Basketball Court Play view</span>
+              {/* TODO render play  */}
+            </div>
+          </ResizablePanel>
+          <ResizableHandle />
+        </ResizablePanelGroup>
+      </div>
+      <div className="mt-10 space-y-10">
+        <TeamsTable />
+        <GamesTable />
+      </div>
     </div>
   );
 }
